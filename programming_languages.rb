97ea08d90d10d2e8aa2fdeec2cb0a6e57a1a882f
Nate Tan <nktan93@gmail.com>
2019-03-12 01:43:00 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,13 +2,17 @@ require 'pry'
 
 def reformat_languages(languages)
   new_hash = {}
+  
   languages.each do |style, language_hash|
     style_array = []
+    
     language_hash.each do |language, type_hash|
       new_hash[language] = type_hash
+      
       unless style_array.include?(style)
         style_array << style
       end
+      
       new_hash[language][:style] = style_array
     end
   end
