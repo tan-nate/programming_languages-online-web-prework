70f1d71a24db9b03d068bfb55489270f9fd4bb97
Nate Tan <nktan93@gmail.com>
2019-03-12 02:00:11 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,92 +1,22 @@
 require 'pry'
-#=begin
-languages = {
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
- 
-  }
-}
-#=end
-def reformat1(languages)
+
+def reformat_languages(languages)
   new_hash = {}
   
-  languages.each do |key, value|
-    value.each do |key, value|
-      new_hash[key] = {}
+  languages.each do |style, language_hash|
+    language_hash.each do |language, type_hash|
+      new_hash[language] = type_hash
+      new_hash[language][:style] = []
+    end
+  end
+  
+  languages.each do |style, language_hash|
+    language_hash.each do |language, type_hash|
+      unless new_hash[language][:style].include?(style)
+        new_hash[language][:style] << style
+      end
     end
   end
   
   new_hash
-  #binding.pry
 end
-
-def reformat2(languages)
-  languages1 = languages
-  #binding.pry
-  new_hash = languages1.collect do |key, value|
-    #binding.pry
-    value.each do |key1, value1|
-      #binding.pry
-      reformat1(languages1).each do |key2, value2|
-        #binding.pry
-        reformat1(languages1)[key2] = value1
-        #binding.pry
-      end
-    end
-    #binding.pry
-  end
-  binding.pry
-  new_hash[1].each do |key, value|
-    new_hash[0][key] = value
-  end
-  binding.pry
-  new_hash[0]
-  binding.pry
-end
-
-def reformat3(languages)
-  reformat2(languages).each do |key1, value1|
-    styles = []
-    
-    languages.each do |key2, value2|
-      if value2.keys.include?(key1)
-        styles << key2
-      end
-      #binding.pry
-    end
-      
-    reformat2(languages)[key1][:style] = [styles].flatten
-    #binding.pry
-  end
-end
-
-def reformat_languages(languages)
-  reformat3(languages)
-end
-
-binding.pry
